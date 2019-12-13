da36f0cc42e3bceb3fa6b5706e6cd016b75674b8
cebrf <cebrf@yandex.ru>
2019-12-13 14:33:54 +1000

    T4 add implementation for DiagnosisController

--- 4_TaskFour/Hospital/Controllers/DiagnosisController.cs
+++ 4_TaskFour/Hospital/Controllers/DiagnosisController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Hospital.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace Hospital.Controllers
 {
@@ -14,13 +15,100 @@ namespace Hospital.Controllers
         {
             this.db = context;
         }
-        public IActionResult Index()
+        public IActionResult Index(string textToFind = null)
         {
-            return View(db.Diagnoses.ToList());
+            if (textToFind == null)
+            {
+                return View(db.Diagnoses.ToList());
+            }
+            else
+            {
+                List<Diagnosis> found = new List<Diagnosis>();
+                foreach (var dia in db.Diagnoses)
+                {
+                    if (dia.Id.ToString().Contains(textToFind))
+                    {
+                        found.Add(dia);
+                    }
+                    else if (dia.Title.Contains(textToFind))
+                    {
+                        found.Add(dia);
+                    }
+                    else if (dia.DeathRate.ToString().Contains(textToFind))
+                    {
+                        found.Add(dia);
+                    }
+                }
+                return View(found);
+            }
         }
+        
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult ShowDetails(int id)
         {
+            if (id == 0 || db.Diagnoses.Find(id) == null)
+            {
+                return NotFound();
+            }
             return View(db.Diagnoses.Find(id));
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Add(Diagnosis diagnosis)
+        {
+            if (diagnosis.Id == 0 || diagnosis.Title == "" || diagnosis.DeathRate == 0 || db.Diagnoses.Find(diagnosis.Id) != null)
+            {
+                return BadRequest();
+            }
+            db.Diagnoses.Add(diagnosis);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Edit(int id)
+        {
+            if (id == 0 || db.Diagnoses.Find(id) == null)
+            {
+                return NotFound();
+            }
+            return View(db.Diagnoses.Find(id));
+        }
+        [HttpPost]
+        public IActionResult Edit(Diagnosis diagnosis)
+        {
+            if (diagnosis.Title == "" || diagnosis.DeathRate == 0)
+            {
+                return BadRequest();
+            }
+            db.Diagnoses.Update(diagnosis);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Delete(int id)
+        {
+            if (id == 0 || db.Diagnoses.Find(id) == null)
+            {
+                return NotFound();
+            }
+            return View(db.Diagnoses.Find(id));
+        }
+        [HttpPost]
+        public IActionResult Delete(Diagnosis diagnosis)
+        {
+            db.Diagnoses.Remove(diagnosis);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
