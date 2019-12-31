f841a60fb0d44b72b085105a6aa9dcd41f44c3ae
cebrf <cebrf@yandex.ru>
2019-12-31 13:28:19 +1000

    T6 task one: change table

--- 6_TaskSix/Task_1/Program.cs
+++ 6_TaskSix/Task_1/Program.cs
@@ -4,15 +4,16 @@ using System.Linq;
 
 namespace Task_1
 {
-    public class User
+    public class Patient
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
+        public string Diagnosis { get; set; }
     }
     public class ApplicationContext : DbContext
     {
-        public DbSet<User> Users { get; set; }
+        public DbSet<Patient> Patients { get; set; }
 
         public ApplicationContext()
         {
@@ -21,7 +22,7 @@ namespace Task_1
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=T_6;Username=postgres;Password=pswrd");
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=T_6;username=postgres;Password=pswrd");
         }
     }
     class Program
@@ -30,24 +31,28 @@ namespace Task_1
         {
             using (ApplicationContext db = new ApplicationContext())
             {
-                // создаем два объекта User
-                User user1 = new User { Name = "Tom", Age = 33 };
-                User user2 = new User { Name = "Alice", Age = 26 };
+                // создаем два объекта Patient
+                Patient Patient1 = new Patient { Name = "Tom", Age = 33, Diagnosis= "tuberculosis" };
+                Patient Patient2 = new Patient { Name = "Mark", Age = 26, Diagnosis = "pneumonia" };
+                Patient Patient3 = new Patient { Name = "Andrew", Age = 26, Diagnosis = "schizophrenia" };
+                Patient Patient4 = new Patient { Name = "Stive", Age = 26, Diagnosis = "blood cancer" };
 
                 // добавляем их в бд
-                if (!db.Users.Any())
+                if (!db.Patients.Any())
                 {
-                    db.Users.Add(user1);
-                    db.Users.Add(user2);
+                    db.Patients.Add(Patient1);
+                    db.Patients.Add(Patient2);
+                    db.Patients.Add(Patient3);
+                    db.Patients.Add(Patient4);
                     db.SaveChanges();
                 }
 
                 // получаем объекты из бд и выводим на консоль
-                var users = db.Users.ToList();
-                Console.WriteLine("Users list:");
-                foreach (User u in users)
+                var Patients = db.Patients.ToList();
+                Console.WriteLine("Patients list:");
+                foreach (Patient u in Patients)
                 {
-                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age} - {u.Diagnosis}");
                 }
             }
         }
