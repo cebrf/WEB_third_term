de87e2de017fdee4bdbdc7bc430e2d1543ae30ed
cebrf <cebrf@yandex.ru>
2020-01-01 14:24:28 +1000

    T6 add task 3

---
+++ 6_TaskSix/Task_3/Migrations/20200101041734_AddField_1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Task_3.Migrations
+{
+    public partial class AddField_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Field_1",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Field_1",
+                table: "Users");
+        }
+    }
+}
---
+++ 6_TaskSix/Task_3/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Task_3
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Field_1 { get; set; }
+    }
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+        {
+            //    Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=T_6_1;username=postgres;Password=pswrd");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Add-Migration Name
+             * Update-Database
+             */
+        }
+    }
+}
